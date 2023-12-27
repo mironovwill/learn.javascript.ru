0fdd97b344796a76fb5e3e02aee1d9ef3d62ca51
Daniel Mironov <lanc3t1337@yandex.com>
2023-12-28 02:04:54 +0300

    Класс 'Spacekraft' (Космический корабль)

---
+++ Задача: Система конструкторов для миссии на Марс/index.js
@@ -0,0 +1,54 @@
+const destinations = {
+  mars: 60,
+  moon: 30,
+  jupiter: 70,
+};
+
+function Spacekraft(name, fuelType) {
+  this.name = name;
+  this.fuelType = fuelType;
+  this.isInSpace = false;
+  this.fuelLevel = 100;
+  this.launch = function () {
+    if (!this.isInSpace) {
+      this.isInSpace = true;
+      this.fuelLevel -= 10;
+      return `${this.name} ready to go`;
+    }
+    return `${this.name} already in space`;
+  };
+  this.land = function (destination) {
+    if (this.isInSpace) {
+      if (destination in destinations) {
+        this.fuelLevel -= destinations[destination];
+        this.isInSpace = false;
+        return `${this.name} landed on ${destination}`;
+      }
+      return `${this.name} cannot land on ${destination}`;
+    }
+
+    return `${this.name} not in space, launch a ship `;
+  };
+  this.checkFuel = function () {
+    return `Current fuel level: ${this.fuelLevel}%`;
+  };
+  this.refuel = function (amount) {
+    if (this.fuelLevel + amount > 100) {
+      this.fuelLevel = 100;
+      return this.checkFuel();
+    }
+
+    this.fuelLevel += amount;
+    return this.checkFuel();
+  };
+}
+
+const abobaAirlines = new Spacekraft("Aboba Airlines", "Diesel");
+console.log(abobaAirlines.land("Donbass"));
+console.log(abobaAirlines.launch());
+console.log(abobaAirlines.land("Donbass"));
+console.log(abobaAirlines.checkFuel());
+console.log(abobaAirlines.land("mars"));
+console.log(abobaAirlines.checkFuel());
+console.log(abobaAirlines.refuel(10));
+console.log(abobaAirlines.refuel(200));
