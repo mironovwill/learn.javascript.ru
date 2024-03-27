6629970de03f9ef31b943f178df42946b85e35aa
Daniel Mironov <lanc3t1337@yandex.com>
2024-03-27 18:07:08 +0300

    9.1 - 9.5

---
+++ 8.4 - 9.5/9.3.js
@@ -0,0 +1,13 @@
+//Класс расширяет объект ?
+
+class Rabbit extends Object {
+    constructor(name) {
+        super()
+        this.name = name;
+    }
+}
+
+let rabbit = new Rabbit("Кроль");
+
+console.log(rabbit.hasOwnProperty('name'));
+
